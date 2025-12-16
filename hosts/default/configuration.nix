28fbfeb02bfa455cc6ec3c12b40be3a86f664766
r <ps04egl@eduvaud.ch>
2025-12-16 19:14:09 +0000

    feat(real cursor)

--- hosts/default/configuration.nix
+++ hosts/default/configuration.nix
@@ -3,72 +3,49 @@
 {
   imports = [ ./hardware-configuration.nix ];
 
-  # ==========================================
-  # 1. CRITICAL HARDWARE FIXES (The "Jitter" Fix)
-  # ==========================================
-  
-  # ThinkPad E14 Gen 7 is too new for the default kernel. 
-  # We MUST use the latest kernel for smooth graphics.
   boot.kernelPackages = pkgs.linuxPackages_latest;
 
-  # Fixes screen stutter/flicker common on ThinkPads (Intel Graphics)
   boot.kernelParams = [ 
     "i915.enable_psr=0" 
   ];
 
-  # Enable Graphics Drivers (OpenGL/Vulkan)
   hardware.graphics = {
     enable = true;
     enable32Bit = true;
     extraPackages = with pkgs; [
-      intel-media-driver   # For Intel GPUs (Broadwell+)
+      intel-media-driver
       libvdpau-va-gl
     ];
   };
 
-  # ==========================================
-  # 2. KEYBOARD (Swiss Fix)
-  # ==========================================
-  
-  # This sets the layout for the console (before you login)
-  console.keyMap = "sg"; # 'sg' is the code for Swiss German in console
+  console.keyMap = "sg";
 
-  # This sets the layout for X11 and Hyprland
   services.xserver.xkb = {
     layout = "ch";
-    variant = "nodeadkeys"; # This makes ~ and ^ type immediately
+    variant = "nodeadkeys";
   };
-
-  # ==========================================
-  # 3. HYPRLAND CONFIGURATION
-  # ==========================================
 
   programs.hyprland = {
     enable = true;
     xwayland.enable = true;
   };
 
-  # Fixes for Electron apps (Obsidian, VSCode) to stop them from lagging
   environment.sessionVariables = {
     NIXOS_OZONE_WL = "1";
-    WLR_NO_HARDWARE_CURSORS = "1"; # Use this if your cursor is invisible or glitchy
-  };
+    WLR_NO_HARDWARE_CURSORS = "1";
 
-  # ==========================================
-  # 4. SYSTEM BASICS
-  # ==========================================
+    XCURSOR_THEME = "Adwaita";
+    XCURSOR_SIZE = "24";
+  };
 
   boot.loader.systemd-boot.enable = true;
   boot.loader.efi.canTouchEfiVariables = true;
 
   networking.networkmanager.enable = true;
+  programs.nm-applet.enable = true;
 
   time.timeZone = "UTC";
   i18n.defaultLocale = "en_US.UTF-8";
-
-  # ==========================================
-  # 5. USER & PACKAGES
-  # ==========================================
 
   users.users.r = {
     isNormalUser = true;
@@ -80,7 +57,7 @@
     enable = true;
     settings = {
       default_session = {
-        command = "${pkgs.hyprland}/bin/Hyprland";
+        command = "Hyprland";
         user = "r";
       };
     };
@@ -90,11 +67,19 @@
   programs.zsh.enable = true;
   services.openssh.enable = true;
 
+  fonts = {
+    fontconfig.enable = true;
+    packages = with pkgs; [
+      noto-fonts-emoji
+      font-awesome
+      powerline-fonts
+      (nerdfonts.override { fonts = [ "JetBrainsMono" ]; })
+    ];
+  };
 
-  
   nixpkgs.config.allowUnfreePredicate = pkg:
     builtins.elem (lib.getName pkg) [ "obsidian" "vscode" ];
-  
+
   environment.systemPackages = with pkgs; [
     hyprland
     waybar
@@ -111,9 +96,14 @@
     firefox
     qbittorrent
     vlc
-
+    networkmanager
+    networkmanagerapplet
+    gnome-power-manager
+    networkmanager_dmenu
     obsidian
     vscode
+
+    adwaita-icon-theme
   ];
 
   system.stateVersion = "25.05";
